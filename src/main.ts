506115e4a34b21bbb589e3456ac9e5dd66a1ca77
unknown <kkw5796@gmail.com>
2022-03-21 11:47:39 +0900

    show log message when server is running

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
+
+const PORT = 3000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const logger = new Logger('Main');
+
+  await app.listen(PORT, () => {
+    logger.log(`Server is runnign on ${PORT}port.`);
+  });
 }
 bootstrap();
